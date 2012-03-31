cc6b31077a522867c77b54fe5cd1e339052c4e68
Miguel Vazquez <miguel.vazquez@fdi.ucm.es>
2012-03-31 16:16:44 +0200

    Add interactions to gene entity

--- lib/rbbt/sources/pina.rb
+++ lib/rbbt/sources/pina.rb
@@ -7,3 +7,15 @@ module Pina
 
   Pina.claim Pina.root.find, :rake, Rbbt.share.install.Pina.Rakefile.find(:lib)
 end
+
+
+
+if defined? Entity and defined? Gene and Entity === Gene
+  module Gene
+    property :pina_interactors => :array2single do 
+      Gene.setup(Pina.protein_protein.tsv(:persist => true, :fields => ["Interactor UniProt/SwissProt Accession"], :type => :flat, :merge => true, :unnamed => true).values_at(*self.uniprot), "UniProt/SwissProt Accession", organism)
+    end
+    persist :_ary_pina_interactors
+  end
+end
+
--- lib/rbbt/sources/string.rb
+++ lib/rbbt/sources/string.rb
@@ -7,3 +7,18 @@ module STRING
 
   STRING.claim STRING.root.find, :rake, Rbbt.share.install.STRING.Rakefile.find(:lib)
 end
+
+if defined? Entity and defined? Gene and Entity === Gene
+  module Gene
+    property :string_interactors => :array2single do |*args|
+      threshold = args.first || 0.85
+      string = STRING.protein_protein.tsv(:unnamed => true, :persist => true)
+      self.ensembl.collect do |gene|
+        interactors = gene.proteins.collect{|protein| Misc.zip_fields((string[protein] || [[],[]])).select{|i, score| score.to_i > threshold}.collect{|ints,s| ints}}.compact.flatten.uniq
+        Protein.setup(interactors, "Ensembl Protein ID", organism).transcript.gene.compact.uniq
+      end
+    end
+    persist :_ary_string_interactors
+  end
+end
+
