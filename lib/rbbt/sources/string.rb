7a6f29c9cee2b4bf166ec18d1747d23c161a08d2
Miguel Vazquez <miguel.vazquez@fdi.ucm.es>
2012-04-23 11:16:28 +0200

    nil

--- lib/rbbt/sources/string.rb
+++ lib/rbbt/sources/string.rb
@@ -13,10 +13,14 @@ if defined? Entity and defined? Gene and Entity === Gene
     property :string_interactors => :array2single do |*args|
       threshold = args.first || 0.85
       string = STRING.protein_protein.tsv(:unnamed => true, :persist => true)
-      self.ensembl.collect do |gene|
+      all = self.ensembl.collect do |gene|
         interactors = gene.proteins.collect{|protein| Misc.zip_fields((string[protein] || [[],[]])).select{|i, score| score.to_i > threshold}.collect{|ints,s| ints}}.compact.flatten.uniq
         Protein.setup(interactors, "Ensembl Protein ID", organism).transcript.gene.compact.uniq
       end
+
+      all.compact.first.annotate all if Annotated === all.compact.first 
+
+      all
     end
     persist :_ary_string_interactors
   end
